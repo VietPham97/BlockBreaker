f78f38989968f74ab7424b70d83458bad8a54d2a
Viet Pham <vietpham1097@gmail.com>
2021-09-16 02:08:29 +0700

    Import assets and add build the first level with physics enable

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    private float screenWidthInUnits = 16f;
+    [SerializeField]
+    private float minX = 1f;
+    [SerializeField]
+    private float manX = 15f;
+
+    private float positionY = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosXInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, positionY);
+        paddlePos.x = Mathf.Clamp(mousePosXInUnits, minX, manX);
+        transform.position = paddlePos;
+    }
+}
